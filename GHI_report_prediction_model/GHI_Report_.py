1b28b42286b1bdde871402535063b191224fb390
Harshal Tapre <harshaltapre27@gmalil.com>
2024-07-08 03:28:32 +0530

    commit message

---
+++ GHI_report_prediction_model/GHI_Report_.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
